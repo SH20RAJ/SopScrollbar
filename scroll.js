c4939b268b8b7009550e375630525ce9b8c799d6
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-01-09 12:07:47 +0000

    More Features Added

--- scroll.js
+++ scroll.js
@@ -1,8 +1,26 @@
 
-
+window.clsn = "blank";
 
 function scfocus(a){
+window.clsn = a;
 document.body.classList = [];
 document.body.classList.add('container');
 document.body.classList.add(a);
+document.getElementById('classname').value = a;
+document.getElementById('code').classList.add(a);
+//showcode(clsn)
+
 }
+
+function showcode(a){
+//console.log(a)
+window.f = "ssb/"+ clsn +".css";
+//console.log(f)
+fetch (f)
+.then(x => x.text())
+.then(y => window.code = y);
+//console.log(code);
+document.getElementById('code').innerText = code;
+}
+
+setInterval(function(){showcode(clsn)},4000);
